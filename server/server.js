6c8cf779813879795803e17266120befde1affe1
traxhcxn <subhash250904@gmail.com>
2025-06-05 21:08:38 +0530

    v1.0.2.2

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ['http://localhost:5173', 'https://workout-server-n7s8.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
 
