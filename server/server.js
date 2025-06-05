dbb374fe413d0001b3db37c9f72e51d7f10f6699
traxhcxn <subhash250904@gmail.com>
2025-06-05 20:55:42 +0530

    v1.0.2.1

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ connectDB()
 app.get('/', (req, res) => {
     res.send('API is running')
 })
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url}`)
+    next()
+})
 app.use('/api', authRoutes)
 app.use('/api/routines', routineRoutes)
 
